353b8daabcb7b027ac4ca5894f1c87fbf25d7622
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-11-10 17:01:31 -0300

    Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Locale.setDefault(Locale.US);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Enter renting information: ");
+        System.out.print("Car model: ");
+        String carModel = sc.nextLine();
+        System.out.print("Withdrawal (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Return (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Enter price per hour: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Enter price per day: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE: ");
+        System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+        sc.close();
+        }
+}
